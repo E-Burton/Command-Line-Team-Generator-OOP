110de38d42ddcccf3b5b3eac5f5ba7e50511d18e
E-Burton <edwina.antoinette@gmail.com>
2020-12-19 12:03:46 -0700

    Initialized empty employees array and added code to push employee type to array for render function; Created addTeamMember function to prompt with start menu after completing appropriate prompts for Manager, Engineer, and Intern function prompts.

--- Develop/app.js
+++ Develop/app.js
@@ -4,8 +4,9 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-// const validator = require("email-validator");
 const joi = require("joi"); // Data Validator
+
+const employees = [];
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -30,23 +31,29 @@ const gatherInfo = () => {
             type: "input",
             message: "What is your manager's email?",
             name: "email",
-            validate: validateEmail,
+            // validate: validateEmail,
         },
         {
             type: "input",
             message: "What is your manager's office number?",
             name: "officeNumber",
         },
-        {
-            type: "list",
-            message: "Which type of team member would you like to add?",
-            name: "employeeType",
-            choices: ["Engineer", "Intern", "I do not want to add any more team members."],
-        }
     ])
     .then((response) => {
-        const manager = new Manager(reponse.name, response.id, reponse.email, response.officeNumber);
+        const manager = new Manager(response.name, response.id, response.email, response.officeNumber);
+        employees.push(manager);
+        addTeamMember();
+    });
+};
 
+const addTeamMember = () => {
+    inquirer.prompt({
+        type: "list",
+        message: "Which type of team member would you like to add?",
+        name: "employeeType",
+        choices: ["Engineer", "Intern", "I do not want to add any more team members."],
+    })
+    .then((response) => {
         switch (response.employeeType) {
             case "Engineer":
                 engineer();
@@ -57,25 +64,15 @@ const gatherInfo = () => {
                 break;
 
             case "I do not want to add any more team members.":
+                fs.writeFileSync(outputPath, render(employees));
                 process.exit();
         }
     });
 };
 
-// Function to validate user input value for email
-const validateEmail = (email) => {
-    const schema = joi.object({
-        email: joi.string()
-            .email({minDomainSegments: 2, tlds: {allow: ["com", "net"]} })
-    });
-    schema.validate(email);
-    return "Please return valid email.";
-}
-
 // Functions to create objects for team members
 
 const engineer = () => {
-    console.log("I'm in the engineer function!");
     inquirer.prompt([
         {
             type: "input",
@@ -91,7 +88,7 @@ const engineer = () => {
             type: "input",
             message: "What is your engineer's email?",
             name: "email",
-            validate: validateEmail,
+            // validate: validateEmail,
         },
         {
             type: "input",
@@ -100,12 +97,15 @@ const engineer = () => {
         }
     ])
     .then((response) => {
-        const engineer = new Engineer(reponse.name, response.id, reponse.email, response.github);
+        const engineer = new Engineer(response.name, response.id, response.email, response.github);
+        employees.push(engineer);
+        console.log(employees);
+        console.log(`You have added ${employees.length - 1} employee(s) to your team total.`);
+        addTeamMember();
     });
 };
 
 const intern = () => {
-    console.log("I'm in the intern function.");
     inquirer.prompt([
         {
             type: "input",
@@ -121,7 +121,6 @@ const intern = () => {
             type: "input",
             message: "What is your intern's email?",
             name: "email",
-            validate: validateEmail,
         },
         {
             type: "input",
@@ -130,30 +129,35 @@ const intern = () => {
         }
     ])
     .then((response) => {
-        const intern = new Intern(reponse.name, response.id, reponse.email, response.school);
+        const intern = new Intern(response.name, response.id, response.email, response.school);
+        employees.push(intern);
+        console.log(employees);
+        console.log(`You have added ${employees.length - 1} employee(s) to your team total.`);
+        addTeamMember();
     });
 };
 
+// Function to validate user input value for email
+const validateEmail = (email) => {
+    const schema = joi.object({
+        email: joi.string()
+            .email({minDomainSegments: 2, tlds: {allow: ["com", "net"]} })
+    });
+    const valid = schema.validate(email);
+    return valid || "Please enter a vaild email address";
+}
 
 // Initialize application/promts for user
 gatherInfo();
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+// After the user has input all employees desired, call the `render` function (required above) and pass in an array containing all employee objects; the `render` function will generate and return a block of HTML including templated divs for each employee!
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
+// `output` folder. You can use the variable `outputPath` above to target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+// HINT: each employee type (manager, engineer, or intern) has slightly different information; write your code to ask different questions via inquirer depending on employee type.
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer, and Intern classes should all extend from a class named Employee; see the directions for further information. Be sure to test out each class and verify it generates an object with the correct structure and methods. This structure will be crucial in order for the provided `render` function to work! ```
--- README.md
+++ README.md
@@ -15,3 +15,4 @@
 ## Additional Resources for Finished Project
 
 * [Joi Validataion](https://joi.dev/api/?v=17.3.0)
+* [Exiting Node Process](https://nodejs.dev/learn/how-to-exit-from-a-nodejs-program)
